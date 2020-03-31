8cfb52ebed8ff7ec547d4936680f09059a9c79c8
bela-varga <kitudja@gmail.com>
2020-03-31 19:49:08 +0200

    updated Tags to look better

--- src/Tags/Tags.tsx
+++ src/Tags/Tags.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './Tags.css';
 
@@ -9,38 +9,30 @@ interface TagsInterface {
 }
 
 const Tags: React.FunctionComponent<TagsInterface> = (props) => {
-  const [showAll, setShowAll] = useState(false);
   const { allTags, usedTags, unusedTags } = props;
 
   return (
-    <div>
-      <h4>
-        Tags
-        <button onClick={() => setShowAll(!showAll)}>Toggle</button>
-      </h4>
-      {
-        showAll &&
+    <div className="tags">
         <div className="flex">
           <div className="all-tags">
-            <h5>All tags</h5>
-            <ul>
-              {!!allTags?.length && allTags.map((tag, index) => <li key={index}>{tag}</li>)}
+            <h4>All tags</h4>
+            <ul className="tag-list">
+              {!!allTags?.length && allTags.map((tag, index) => <li className="tag" key={index}>{tag}</li>)}
             </ul>
           </div>
           <div className="used-tags">
-            <h5>Used tags</h5>
-            <ul>
-              {!!usedTags?.length && usedTags.map((tag, index) => <li key={index}>{tag}</li>)}
+            <h4>Used tags</h4>
+            <ul className="tag-list">
+              {!!usedTags?.length && usedTags.map((tag, index) => <li className="tag" key={index}>{tag}</li>)}
             </ul>
           </div>
           <div className="unused-tags">
-            <h5>Not used tags</h5>
-            <ul>
-              {!!unusedTags?.length && unusedTags.map((tag, index) => <li key={index}>{tag}</li>)}
+            <h4>Not used tags</h4>
+            <ul className="tag-list">
+              {!!unusedTags?.length && unusedTags.map((tag, index) => <li className="tag" key={index}>{tag}</li>)}
             </ul>
           </div>
         </div>
-      }
     </div>
   )
 }
