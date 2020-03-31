dbccb7e1903ea2833cd124fdc51929a5da065976
bela-varga <kitudja@gmail.com>
2020-03-31 23:21:10 +0200

    added demo folder with current build

---
+++ demo/precache-manifest.911f29b48a1684461ed304ae8a6ba3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413774eeee886b3797e35a4a2bde7fdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98b8a0e7ce801049f852",
+    "url": "/static/css/main.8cd2e714.chunk.css"
+  },
+  {
+    "revision": "a4d8c56a02c50057ad4c",
+    "url": "/static/js/2.c3818e5f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c3818e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b8a0e7ce801049f852",
+    "url": "/static/js/main.bb479ced.chunk.js"
+  },
+  {
+    "revision": "c63c240bfb9d3e5a6b28",
+    "url": "/static/js/runtime-main.3a61e3da.js"
+  }
+]);
